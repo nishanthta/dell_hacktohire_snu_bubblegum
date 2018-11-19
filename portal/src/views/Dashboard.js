cbadca25f5a32a23cf92ccbe733b6ec4bd2971dd
kanishk98 <kanishk.kakar@gmail.com>
2018-11-19 16:15:11 +0530

    Init frontend

---
+++ portal/src/views/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Redirect, { browserHistory } from 'react-router';
+
+export default class Dashboard extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+    
+}
