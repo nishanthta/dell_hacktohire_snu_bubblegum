057565571b1269091a34780fc82fc0a79edfd0c7
kanishk98 <kanishk.kakar@gmail.com>
2018-11-19 21:21:53 +0530

    Set up basic routing

--- portal/src/App.js
+++ portal/src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './views/Dashboard';
+import Header from './views/Header';
+import Main from './views/Main';
 
 class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <div>
+        <Header />
+        <Main />
+      </div>
     );
   }
 }
