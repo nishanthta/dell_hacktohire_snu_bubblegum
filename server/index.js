b3020439b68a483301adf111878744a146f37923
kanishk98 <kanishk.kakar@gmail.com>
2018-11-19 16:45:18 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -29,6 +29,11 @@ client.connect()
     const express = require('express');
     const app = express();
     app.use(express.json());
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
     app.post('/add-item', (req, res) => {
         const location = req.query.location;
