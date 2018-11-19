ac2d587a6b6459dc4f1abafd96abda3a8b1aca37
kanishk98 <kanishk.kakar@gmail.com>
2018-11-19 21:27:01 +0530

    API fetching working

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Dashboard from './views/Dashboard';
 import Header from './views/Header';
 import Main from './views/Main';
 
--- portal/src/views/Dashboard.js
+++ portal/src/views/Dashboard.js
@@ -6,59 +6,27 @@ export default class Dashboard extends React.PureComponent {
     super(props);
     this.state = {
       itemData: null,
-      cols: null
     }
   }
 
   componentDidMount() {
-    /*const url = 'http://' + '18.188.218.137' + '/get-items?location=delhi';
+    const url = 'http://18.188.218.137/get-items?location=delhi';
     console.log(url);
     fetch(url)
     .then(async (res) => {
       console.log(res);
       let data = await res.json();
       console.log(data);
-      let cols = [];
-      for (let d in data) {
-        let pt = Object.keys(data[d]);
-        cols.push(pt);
-      }
-      console.log(cols);
-      let temp = [];
-      for (let index in cols) {
-        let col = {};
-        col['Header'] = cols[index];
-        temp.push(col);
-      }
-      console.log(temp);
-      temp = [];
-      for (let index in data) {
-        let obj = {};
-        obj = data[index];
-        temp.push(obj);
-      }
-      this.setState({retrieved: true, itemData: data, cols: temp});*/
-    const data = [{
-      name: 'Tanner Linsley',
-      age: 26,
-    }, {
-      name: 'Tandner Linsley',
-      age: 26,
-    }];
-
-    const columns = [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
-    }, {
-      Header: 'Age',
-      accessor: 'age',
-    }];
-    this.setState({ itemData: data, cols: columns });
+      this.setState({itemData: data});
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
   render() {
     console.log(this.state);
-    if (!this.state.itemData || !this.state.cols) {
+    if (!this.state.itemData) {
       return null;
     }
     return (
